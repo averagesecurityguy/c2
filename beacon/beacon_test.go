d3eafc8c4aae6c8b406a7855f52431e4d193eae7
Stephen Haywood <stephen@averagesecurityguy.info>
2018-01-29 23:33:05 -0500

    go fmt

--- beacon/beacon_test.go
+++ beacon/beacon_test.go
@@ -1,28 +1,28 @@
 package beacon
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestHttpBeacon(t *testing.T) {
-    h := NewHttpAuthBeacon("uuid", "http://127.0.0.1:8000/", "agent")
-    url := h.Ping()
+	h := NewHttpAuthBeacon("uuid", "http://127.0.0.1:8000/", "agent")
+	url := h.Ping()
 
-    if url == "" {
-        t.Error("Did not receive a URL from the server.")
-    }
+	if url == "" {
+		t.Error("Did not receive a URL from the server.")
+	}
 }
 
 func TestDnsBeacon(t *testing.T) {
-    ns := "ns.domain.com."
-    d := NewDnsNsBeacon("uuid", "domain.com")
-    host := d.Ping()
+	ns := "ns.domain.com."
+	d := NewDnsNsBeacon("uuid", "domain.com")
+	host := d.Ping()
 
-    if host == "" {
-        t.Error("Did not receive a hostname from the server.")
-    }
+	if host == "" {
+		t.Error("Did not receive a hostname from the server.")
+	}
 
-    if host != ns {
-        t.Error("Expected", ns, "got", host)
-    }
+	if host != ns {
+		t.Error("Expected", ns, "got", host)
+	}
 }
