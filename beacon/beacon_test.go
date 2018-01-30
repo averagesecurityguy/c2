31eb026a21be70c7a369ec9755ceca8d45ad95b1
Stephen Haywood <stephen@averagesecurityguy.info>
2018-01-29 23:07:18 -0500

    Add test for DNS beacon.

--- beacon/beacon_test.go
+++ beacon/beacon_test.go
@@ -4,11 +4,25 @@ import (
     "testing"
 )
 
-func TestBeacon(t *testing.T) {
+func TestHttpBeacon(t *testing.T) {
     h := NewHttpAuthBeacon("uuid", "http://127.0.0.1:8000/", "agent")
     url := h.Ping()
 
     if url == "" {
         t.Error("Did not receive a URL from the server.")
+    }
+}
+
+func TestDnsBeacon(t *testing.T) {
+    cname := "cname.domain.com"
+    d := NewDnsCnameBeacon("uuid", "domain.com")
+    host := d.Ping()
+
+    if host == "" {
+        t.Error("Did not receive a hostname from the server.")
+    }
+
+    if host != cname {
+        t.Error("Expected", cname, "got", host)
     }
 }
